4d39d7fb224fb9d8413bef6b957f5a4fe051466f
Darshan <darshantp13@gmail.com>
2026-07-19 14:50:31 +0530

    feat(transforms): add RandAugment, RandomErasing, IMAGE_SIZE constant, and stronger minority transform

--- src/kneevision/data/transforms.py
+++ src/kneevision/data/transforms.py
@@ -1,40 +1,47 @@
 from torchvision import transforms
 
-# Minority classes (KL-3, KL-4) get stronger augmentation
+IMAGE_SIZE = 224
+
+randaugment = transforms.RandAugment(num_ops=2, magnitude=9)
+
+train_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.RandomResizedCrop((IMAGE_SIZE, IMAGE_SIZE), scale=(0.8, 1.0)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(degrees=15),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),
+    randaugment,
+    transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
+    transforms.RandomAdjustSharpness(sharpness_factor=2, p=0.3),
+    transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 0.5)),
+    transforms.ToTensor(),
+    transforms.RandomErasing(p=0.25, scale=(0.02, 0.15)),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
 minority_transform = transforms.Compose([
     transforms.Resize((256, 256)),
-    transforms.RandomResizedCrop((224, 224), scale=(0.7, 1.0)),
+    transforms.RandomResizedCrop((IMAGE_SIZE, IMAGE_SIZE), scale=(0.7, 1.0)),
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(25),
     transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.2, hue=0.1),
+    randaugment,
     transforms.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.85, 1.15), shear=10),
     transforms.RandomGrayscale(p=0.1),
     transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 1.0)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-
-train_transform = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.RandomResizedCrop((224, 224), scale=(0.8, 1.0)),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomRotation(degrees=15),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2),
-    transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
-    transforms.RandomAdjustSharpness(sharpness_factor=2, p=0.2),
-    transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 0.5)),
-    transforms.ToTensor(),
+    transforms.RandomErasing(p=0.3, scale=(0.02, 0.2)),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 val_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 base_tta = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
@@ -42,7 +49,7 @@ base_tta = transforms.Compose([
 tta_transforms_list = [
     base_tta,
     transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
         transforms.RandomHorizontalFlip(p=1.0),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
